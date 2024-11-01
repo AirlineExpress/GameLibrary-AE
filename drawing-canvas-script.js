d917eb135bcc4ae0ea2021296a14ef9f607ecccc
AirlineExpress <123339883+AirlineExpress@users.noreply.github.com>
2024-11-01 11:38:05 -0500

    Updated mobile compatibility
    Removed double tap zoom, added mobile drawing support

--- drawing-canvas-script.js
+++ drawing-canvas-script.js
@@ -7,6 +7,10 @@ canvas.addEventListener('mousedown', startDrawing);
 canvas.addEventListener('mouseup', stopDrawing);
 canvas.addEventListener('mousemove', draw);
 
+canvas.addEventListener('touchstart', startDrawing);
+canvas.addEventListener('touchend', stopDrawing);
+canvas.addEventListener('touchmove', draw);
+
 function startDrawing(e) {
     drawing = true;
     draw(e);
@@ -17,16 +21,31 @@ function stopDrawing() {
     ctx.beginPath();
 }
 
+function getTouchPos(canvas, touchEvent) {
+    const rect = canvas.getBoundingClientRect();
+    return {
+        x: touchEvent.touches[0].clientX - rect.left,
+        y: touchEvent.touches[0].clientY - rect.top
+    };
+}
+
 function draw(e) {
     if (!drawing) return;
+    e.preventDefault(); // Prevent scrolling on touch devices
+    let pos;
+    if (e.type === 'mousemove') {
+        pos = { x: e.clientX - canvas.offsetLeft, y: e.clientY - canvas.offsetTop };
+    } else {
+        pos = getTouchPos(canvas, e);
+    }
     ctx.lineWidth = 5;
     ctx.lineCap = 'round';
     ctx.strokeStyle = '#000000'; // Black color
 
-    ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    ctx.lineTo(pos.x, pos.y);
     ctx.stroke();
     ctx.beginPath();
-    ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    ctx.moveTo(pos.x, pos.y);
 }
 
 function clearCanvas() {
